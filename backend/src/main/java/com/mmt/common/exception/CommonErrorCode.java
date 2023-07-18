9f5f55aff485daeb26d0d265f7e7c3f4c25d5046
byunyc0124 <baeyonyk124@gmail.com>
2023-07-18 16:43:08 +0900

    [STYLE] 에러 코드 메세지 자세하게 수정 #S09P12C111-82

--- backend/src/main/java/com/mmt/common/exception/CommonErrorCode.java
+++ backend/src/main/java/com/mmt/common/exception/CommonErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode{
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 자원입니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "존재하지 않는 자원입니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "존재하지 않는 자원입니다.");
+    FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "유효한 인증이 아닙니다.");
 
     private final HttpStatus resultCode;
     private final String resultMessage;
