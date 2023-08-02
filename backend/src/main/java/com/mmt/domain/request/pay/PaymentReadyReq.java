a18729065888eacd9a14214bd83797baf5a1e127
jjoyra <bunny7531@naver.com>
2023-08-02 22:54:46 +0900

    [MODIFY] 결제 준비 API path 수정 #S09P12C111-225

--- backend/src/main/java/com/mmt/domain/request/pay/PaymentReadyReq.java
+++ backend/src/main/java/com/mmt/domain/request/pay/PaymentReadyReq.java
@@ -1,8 +1,11 @@
 package com.mmt.domain.request.pay;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class PaymentReadyReq {
     private String userId;
     private int lessonId;
