006f4f796d0a5f303ec55a83416383a9cada4991
hwnagsua <tsmich926@naver.com>
2023-08-06 11:17:49 +0900

    [FEAT]리뷰상세보기 #S09P12C111-250

--- frontend/src/component/MyPageT/Review.js
+++ frontend/src/component/MyPageT/Review.js
@@ -1,124 +1,39 @@
-// import React, { useState, useEffect } from "react";
-// import SideBar from './SideBar';
-// import { useSelector} from "react-redux";
-// import axios from "axios";
-
-// function Review(props) {
-//   const accessToken = useSelector((state) => state.auth.access_token);
-//   const cookyerId = useSelector((state) => state.auth.id);
-//   const [reviews, setReviews] = useState([]);
-//   const [grade, setGrade] = useState([]);
-//   const gradeMessage = grade.count === 0 ? "받은 별점이 없습니다." : "";
-//   const reviewsMessage = reviews[0]?.lessonId === 0 ? "받은 리뷰가 없습니다." : "";
-
-//     //평점 불러오기
-//     useEffect(() => {
-//       axios
-//         .get(`api/v1/review/avg/${cookyerId}`, {
-//           headers: {
-//             Access_Token: accessToken,
-//           },
-//         })
-//         .then((res) => {
-//           setGrade(res.data);
-//           console.log("평점",grade);
-//         })
-//         .catch((err) => {
-//           console.log("평점에러",err);
-//         });
-//     }, [accessToken,grade]);
-
-//     //리뷰 불러오기
-//     useEffect(() => {
-//       axios
-//         .get(`api/v1/review/${cookyerId}`, {
-//           headers: {
-//             Access_Token: accessToken,
-//           },
-//         })
-//         .then((res) => {
-//           setReviews(res.data);
-//           console.log("리뷰목록",reviews);
-//         })
-//         .catch((err) => {
-//           console.log("평점에러",err);
-//         });
-//     }, [accessToken,reviews]);
-
-//   return (
-//     <div>
-//       <SideBar />
-//       <section>
-//         <div className="header">
-//           <h2 className="header_title">받은 리뷰</h2>
-//         <h2 className="section_title">
-//           평균별점:
-//         </h2>
-//         <div>
-//         </div>
-//         </div>
-//         <ul className="caution_list">
-//           <div className="caution_list_item">
-//             <div>
-//             </div>
-//           </div>
-//         </ul>
-//         <section className="review">
-//         {reviewsMessage ? (
-//           <div className="noreview">{reviewsMessage}</div>
-//         ) : (
-//           // 리뷰 있는 경우
-//           reviews.map((review) => (
-//             <div className="pay_item" key={review.id}>
-//           <div className="review_box">
-//             <div className="review_item">
-//               <div className="review_cont">
-//                 <a href="dd" className="review_link">강좌이름</a>
-//                 <div className="review_star">
-//                   ⭐️⭐️⭐️⭐️ 4.2
-//                 </div>
-//                 <div className="review_author">
-//                   작성자/아이디
-//                 </div>
-//                 <div className="review_tutor">
-//                   선생님닉네임/이름
-//                 </div>
-//                 <div className="review_cont">
-//                   리뷰내용
-//                   <div className="review_cont">
-//                     맛있었구요.....
-//                   </div>
-//                 </div>
-//                 <div className="review_fun">
-//                   <button type="button" className="review_btn">
-//                     <i className="review_icon">🔍</i>
-//                     <span className="review_btn_txt">자세히보기</span>
-//                     </button>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             ))
-//           )}
-//         </section>
-//       </section>
-//     </div>
-//   );
-// }
-
-// export default Review;
-
 import React, { useState, useEffect } from "react";
 import SideBar from "./SideBar";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import Modal from 'react-modal';
+import ReviewDetailT from "./ReviewDetailT";
 
-function Review(props) {
+
+
+function Review() {
   const accessToken = useSelector((state) => state.auth.access_token);
   const cookyerId = useSelector((state) => state.auth.id);
   const [reviews, setReviews] = useState([]);
   const [grade, setGrade] = useState([]);
   const reviewsMessage = reviews[0]?.lessonId === 0 ? "받은 리뷰가 없습니다." : "";
+  //모달관련
+  const [selectedReviewId, setSelectedReviewId] = useState(null); // 선택된 리뷰의 reviewId
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+  const handleOpenModal = (reviewId) => {
+    setSelectedReviewId(reviewId); // 선택한 리뷰의 reviewID저장
+    setIsModalOpen(true);
+  };
+
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    Modal.setAppElement("#root"); 
+  }, []);
+
+
+
 
   //평점 불러오기
   useEffect(() => {
@@ -188,7 +103,7 @@ function Review(props) {
                       <div className="review_cont">{review.content}</div>
                     </div>
                     <div className="review_fun">
-                      <button type="button" className="review_btn">
+                      <button type="button" className="review_btn" onClick={() =>handleOpenModal(review.reviewId)}>
                         <i className="review_icon">🔍</i>
                         <span className="review_btn_txt">자세히보기</span>
                       </button>
@@ -198,6 +113,9 @@ function Review(props) {
               </div>
             ))
           )}
+          <Modal isOpen={isModalOpen} onRequestClose={handleCloseModal}>
+            <ReviewDetailT reviewId={selectedReviewId} onClose={handleCloseModal} />
+          </Modal>
         </section>
       </section>
     </div>
