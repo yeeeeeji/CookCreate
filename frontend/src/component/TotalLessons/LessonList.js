84bb21d366235946b04f60838803662d7a8641ea
양수원 <classicblue205@gmail.com>
2023-08-08 21:43:47 +0900

    [MODIFY] 비로그인시 과외 상세보기 불가능으로 수정 #S09P12C111-238

--- frontend/src/component/TotalLessons/LessonList.js
+++ frontend/src/component/TotalLessons/LessonList.js
@@ -1,30 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import LessonItem from './LessonItem';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
-import '../../style/lesson/lessonListCss.css';
 import { useNavigate } from 'react-router-dom';
+import LessonItem from './LessonItem';
+import '../../style/lesson/lessonListCss.css';
+
 function LessonList() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [lessons, setLessons] = useState([]);
   const type = useSelector((state) => state.lessonSearch.type);
   const deadline = useSelector((state) => state.lessonSearch.deadline);
   const order = useSelector((state) => state.lessonSearch.order);
   const category = useSelector((state) => state.lessonSearch.category);
   const keyword = useSelector((state) => state.lessonSearch.keyword);
-  const isLogin = useSelector((state) => state.auth.isLogin)
-  
+  const isLogin = useSelector((state) => state.auth.isLogin);
+
   const handleLessonDetail = (lessonId) => {
-    navigate(`/lesson/${lessonId}`)
-  }
+    navigate(`/lesson/${lessonId}`);
+  };
 
   const gotoLogin = () => {
-    alert('로그인 후 확인할 수 있습니다!')
-    navigate('/login')
-  }
+    alert('로그인 후 확인할 수 있습니다!');
+    navigate('/login');
+  };
+
   useEffect(() => {
-    axios.get(`/api/v1/lesson`, {
+    axios.get('/api/v1/lesson', {
       params: {
         type,
         keyword,
@@ -34,34 +35,24 @@ function LessonList() {
       }
     })
     .then((res) => {
-      setLessons(res.data)
+      setLessons(res.data);
     })
     .catch((err) => {
-      console.error(err)
+      console.error(err);
     });
-  }, [type, keyword, category, order, deadline])
-  
+  }, [type, keyword, category, order, deadline]);
+
   return (
-    <div className='lessonListContainer'>
+    <div className="lessonListContainer">
       {lessons.map((lesson) => (
-        <div 
+        <div
           key={lesson.lessonId}
           className="lessonItemContainer"
-        >
-            <LessonItem
-              id={lesson.lessonId}
-              title={lesson.lessonTitle}
-              date={lesson.lessonDate}
-              thumbnailUrl = {lesson.thumbnailUrl}
-              reviewAvg = {lesson.reviewAvg}
-              cookyerName = {lesson.cookyerName}
-              categoryId = {lesson.categoryId} 
-            />
-          key={lesson.lessonId}
           style={{
             border: '1px solid #ccc',
             padding: '20px',
-            marginTop: '20px'
+            marginTop: '20px',
+            cursor: 'pointer',
           }}
           onClick={() => {
             if (isLogin) {
@@ -69,7 +60,7 @@ function LessonList() {
             } else {
               gotoLogin();
             }
-          }}  
+          }}
         >
           <LessonItem
             id={lesson.lessonId}
@@ -78,7 +69,7 @@ function LessonList() {
             thumbnailUrl={lesson.thumbnailUrl}
             reviewAvg={lesson.reviewAvg}
             cookyerName={lesson.cookyerName}
-            categoryId={lesson.categoryId} 
+            categoryId={lesson.categoryId}
           />
         </div>
       ))}
