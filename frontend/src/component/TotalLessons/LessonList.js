bfde75d572f5a985cf5c45920142adfd8b1ac286
양수원 <classicblue205@gmail.com>
2023-08-08 21:34:46 +0900

    [MODIFY] 유저가 아닌 경우 과외 상세보기 불가능으로 수정 및 로그인 화면으로 이동 #S09P12C111-238

--- frontend/src/component/TotalLessons/LessonList.js
+++ frontend/src/component/TotalLessons/LessonList.js
@@ -3,15 +3,25 @@ import { Link } from 'react-router-dom';
 import LessonItem from './LessonItem';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 function LessonList() {
+  const navigate = useNavigate()
   const [lessons, setLessons] = useState([]);
   const type = useSelector((state) => state.lessonSearch.type);
   const deadline = useSelector((state) => state.lessonSearch.deadline);
   const order = useSelector((state) => state.lessonSearch.order);
   const category = useSelector((state) => state.lessonSearch.category);
   const keyword = useSelector((state) => state.lessonSearch.keyword);
+  const isLogin = useSelector((state) => state.auth.isLogin)
+  
+  const handleLessonDetail = (lessonId) => {
+    navigate(`/lesson/${lessonId}`)
+  }
 
+  const gotoLogin = () => {
+    alert('로그인 후 확인할 수 있습니다!')
+    navigate('/login')
+  }
   useEffect(() => {
     axios.get(`/api/v1/lesson`, {
       params: {
@@ -29,31 +39,34 @@ function LessonList() {
       console.error(err)
     });
   }, [type, keyword, category, order, deadline])
-
+  
   return (
     <div>
       {lessons.map((lesson) => (
         <div 
-        key={lesson.lessonId}
-        style={{
-          border: '1px solid #ccc',
-          padding: '20px',
-          marginTop: '20px'
-          }}>
-          <Link
-            to={`/lesson/${lesson.lessonId}`}
-            style={{ textDecoration: 'none', color: 'inherit' }}
-          >
-            <LessonItem
-              id={lesson.lessonId}
-              title={lesson.lessonTitle}
-              date={lesson.lessonDate}
-              thumbnailUrl = {lesson.thumbnailUrl}
-              reviewAvg = {lesson.reviewAvg}
-              cookyerName = {lesson.cookyerName}
-              categoryId = {lesson.categoryId} 
-            />
-          </Link>
+          key={lesson.lessonId}
+          style={{
+            border: '1px solid #ccc',
+            padding: '20px',
+            marginTop: '20px'
+          }}
+          onClick={() => {
+            if (isLogin) {
+              handleLessonDetail(lesson.lessonId);
+            } else {
+              gotoLogin();
+            }
+          }}  
+        >
+          <LessonItem
+            id={lesson.lessonId}
+            title={lesson.lessonTitle}
+            date={lesson.lessonDate}
+            thumbnailUrl={lesson.thumbnailUrl}
+            reviewAvg={lesson.reviewAvg}
+            cookyerName={lesson.cookyerName}
+            categoryId={lesson.categoryId} 
+          />
         </div>
       ))}
     </div>
