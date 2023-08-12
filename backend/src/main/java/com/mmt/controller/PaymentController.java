1f31ec9b65857484d6239438a697789dd71cff72
jjoyra <bunny7531@naver.com>
2023-08-12 21:37:49 +0900

    [CHORE] 배포용 결제 리다이렉트 uri 변경

--- backend/src/main/java/com/mmt/controller/PaymentController.java
+++ backend/src/main/java/com/mmt/controller/PaymentController.java
@@ -58,8 +58,10 @@ public class PaymentController {
     @GetMapping("/completed")
     public RedirectView approvePay(String pg_token, int paymentId) {
         MyPaymentRes myPaymentRes = paymentService.approvePay(pg_token, paymentId);
-        String redirectURL = "http://i9c111.p.ssafy.io/payment/success?paymentId=" + paymentId
+        String redirectURL = "http://i9c111.p.ssafy.io/payment/fail?paymentId=" + paymentId
                 +"&payStatus=" + PayStatus.COMPLETED;
+//        String redirectURL = "http://localhost:3000/payment/success?paymentId=" + paymentId
+//                +"&payStatus=" + PayStatus.COMPLETED;
         return new RedirectView(redirectURL);
     }
 
@@ -67,10 +69,11 @@ public class PaymentController {
     @GetMapping("/fail")
     public RedirectView failPay(int paymentId) {
         paymentService.failPay(paymentId);
-
         String redirectURL = "http://i9c111.p.ssafy.io/payment/fail?paymentId=" + paymentId
                 +"&payStatus=" + PayStatus.FAIL;
-        return new RedirectView(redirectURL);
+//        String redirectURL = "http://localhost:3000/payment/fail?paymentId=" + paymentId
+//                +"&payStatus=" + PayStatus.FAIL;
+//        return new RedirectView(redirectURL);
     }
 
     @Parameter(hidden = true)
@@ -78,9 +81,10 @@ public class PaymentController {
     public RedirectView cancelPay(int paymentId) {
         // TODO: 취소 처리
         paymentService.cancelPay(paymentId);
-
-        String redirectURL = "http://i9c111.p.ssafy.io/payment/cancel?paymentId=" + paymentId
+        String redirectURL = "http://i9c111.p.ssafy.io/payment/fail?paymentId=" + paymentId
                 +"&payStatus=" + PayStatus.CANCEL;
+//        String redirectURL = "http://localhost:3000/payment/cancel?paymentId=" + paymentId
+//                +"&payStatus=" + PayStatus.CANCEL;
         return new RedirectView(redirectURL);
     }
 
