3a15e0d05df2c86a22f38f073be2cb75b898de4d
hwnagsua <tsmich926@naver.com>
2023-08-13 18:10:47 +0900

    [FEAT]채팅입장 메세지수정 #S09P12C111-180

--- frontend/src/component/ChatRoom/ChatBlock.js
+++ frontend/src/component/ChatRoom/ChatBlock.js
@@ -1,3 +1,163 @@
+// import React from "react";
+// import styled from "styled-components";
+
+// const BorderBlock = styled.div`
+//   position: relative;
+//   text-align: center;
+//   width: 100%;
+//   padding: 13px 0;
+//   & span {
+//     position: relative;
+//     display: inline-block;
+//     // background-color: #b2c7d9;
+//     // background-color: #d1cfca;
+//     background-color: #ffd3a1;
+//     padding: 0 10px;
+//   }
+//   // &:before {
+//   //   content: "";
+//   //   display: block;
+//   //   position: absolute;
+//   //   left: 2%;
+//   //   top: 50%;
+//   //   width: 96%;
+//   //   height: 1px;
+//   //   background-color: #727b83;
+//   // }
+// `;
+
+// const ChatWrapper = styled.div`
+//   position: relative;
+//   display: inline-block;
+//   padding: 7px 8px;
+//   border-radius: 4px;
+//   margin-bottom: 7px;
+//   box-shadow: 0px 1px 2px 0px #8fabc7;
+//   max-width: 70%;
+//   word-wrap: break-word;
+//   white-space: pre-wrap;
+// `;
+// const RightBlock = styled.div`
+//   text-align: right;
+//   margin-top: 10px;
+//   margin-left: 10px;
+//   margin-right: 10px;
+
+//   & ${ChatWrapper} {
+//     // background-color: #ffec42;
+//     background-color: #ffffff;
+
+//     text-align: left;
+//     & span {
+//       position: absolute;
+//       display: inline-block;
+//       &.time {
+//         min-width: 65px;
+//         text-align: right;
+//         bottom: 0;
+//         left: -70px;
+//       }
+//       &.not-read {
+//         color: #ffec42;
+//         min-width: 30px;
+//         text-align: right;
+//         bottom: 16px;
+//         left: -35px;
+//       }
+//     }
+//   }
+// `;
+// const LeftBlock = styled.div`
+//   position: relative;
+//   margin-top: 10px;
+//   margin-left: 10px;
+//   margin-right: 10px;
+//   padding-left: 50px;
+//   & ${ChatWrapper} {
+//     // background-color: #fff;
+//     background-color: #ffe3b0;
+
+//     & span {
+//       position: absolute;
+//       display: inline-block;
+//       &.time {
+//         min-width: 65px;
+//         text-align: left;
+//         bottom: 0;
+//         right: -70px;
+//       }
+//       &.not-read {
+//         color: #ffec42;
+//         min-width: 30px;
+//         text-align: left;
+//         bottom: 16px;
+//         right: -35px;
+//       }
+//     }
+//   }
+//   & img {
+//     position: absolute;
+//     top: 3px;
+//     left: 0;
+//     height: 45px;
+//     width: 45px;
+//     border-radius: 20px;
+//     float: left;
+//     cursor: pointer;
+//   }
+// `;
+
+// const Chat = ({ message, author}) => {
+//   return (
+//     <ChatWrapper>
+//       <span className="username">{author}</span>
+//       {message}
+//       {/* <span className="time">{localeTime}</span> */}
+//       {/* <span className="not-read">{notRead > 1 ? notRead : ""}</span> */}
+//     </ChatWrapper>
+//   );
+// };
+
+// const SeparationBlock = ({ EnterNickname }) => {
+//   return (
+//     <BorderBlock>
+//       <span>{EnterNickname}님이 입장하셨습니다.</span>
+//     </BorderBlock>
+//   );
+// };
+
+// const MyChat = ({ content, ...props }) => {
+//   return (
+//     <React.Fragment>
+//       {content ? <SeparationBlock content={content} /> : null}
+//       <RightBlock>
+//         <div>
+//           <Chat {...props} />
+//         </div>
+//       </RightBlock>
+//     </React.Fragment>
+//   );
+// };
+
+// const FriendChat = ({ author, message }) => {
+//   return (
+//     <LeftBlock>
+//       <div>
+//         <span className="username">{author}</span>
+//         <Chat message={message} />
+//       </div>
+//     </LeftBlock>
+//   );
+// };
+
+// export { Chat, MyChat, FriendChat, SeparationBlock };
+
+
+
+
+
+
+
 import React from "react";
 import styled from "styled-components";
 
@@ -129,10 +289,10 @@ const Chat = ({ message, author}) => {
   );
 };
 
-const SeparationBlock = ({ EnterMessage }) => {
+const SeparationBlock = ({ EnterNickname }) => {
   return (
     <BorderBlock>
-      <span>{EnterMessage}</span>
+      <span>{EnterNickname}</span>
     </BorderBlock>
   );
 };
@@ -150,7 +310,7 @@ const MyChat = ({ content, ...props }) => {
   );
 };
 
-const FriendChat = ({ author, message}) => {
+const FriendChat = ({ author, message }) => {
   return (
     <LeftBlock>
       <div>
