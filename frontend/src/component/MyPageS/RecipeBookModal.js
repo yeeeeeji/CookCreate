615ea4529f081e454aab1e6649d7cb073d334a85
윤예지 <hallo27@naver.com>
2023-08-14 19:20:38 +0900

    [CSS] 쿠키 마이페이지 레시피북 모달 CSS 적용

--- frontend/src/component/MyPageS/RecipeBookModal.js
+++ frontend/src/component/MyPageS/RecipeBookModal.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import '../../style/video/videoModals.css'
 
 function RecipeBookModal({ lessonStepList, handleShowRecipe }) {
 
   return (
-    <div className='recipe-book-modal'>
-      <h1>레시피</h1>
-      {lessonStepList ? (
-        lessonStepList.map((step) => (
-          <p>{step.stepOrder}. {step.stepContent}</p>
-        ))
-      ) : null}
-      <button onClick={() => handleShowRecipe({showRecipe: false, recipe: null})}>확인</button>
+    <div className='cookyer-lesson-step-modal'>
+      <div className='cookyer-lesson-step-modal-title-wrapper'>
+        <p className='cookyer-lesson-step-modal-title'>레시피</p>
+      </div>
+      <div className='cookyer-lesson-step-modal-steps'>
+        <div className='cookyer-lesson-step-modal-container'>
+          {lessonStepList ? (
+            lessonStepList.map((step) => (
+              <div className='cookyer-lesson-step-modal-step'>
+                <p className='cookyer-lesson-step-modal-step-order'>{step.stepOrder}.</p>
+                <p className='cookyer-lesson-step-modal-step-content'>{step.stepContent}</p>
+              </div>
+            ))
+          ) : null}
+        </div>
+      </div>
+      <div className='cookyer-lesson-step-modal-btn'>
+        <button onClick={() => handleShowRecipe({showRecipe: false, recipe: null})}>확인</button>
+      </div>
     </div>
   );
 }
