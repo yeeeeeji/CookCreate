5ac85361f3b88eabf06cb1c3660efef8dfd69e2a
Phabala(Kim Gihong) <kihong2424@gmail.com>
2023-08-14 17:48:22 +0900

    [STYLE] 설명 부분 CSS 변경

--- frontend/src/component/Gesture/TestExplain.js
+++ frontend/src/component/Gesture/TestExplain.js
@@ -10,10 +10,9 @@ function TestExplain() {
                     <div>👌 오케이 표시로 타이머를 시작해 보아요!</div>
                     <div>✅ 완료되면 엄지와 검지를 펴서 체크업 하세요!</div>
                     <div>✋ 질문이 있으면 손을 번쩍 들어 질문하세요!</div>
-                    <br/>
-                    <pre>       다시 놀고싶으면 Reset 버튼을 누르세요</pre>
                     {/* <div>(설명 CSS변경, 홈 되돌아가기 버튼 추가 예정)</div> */}
                 </div>
+                <div className="test-explain-reset">다시 놀고싶으면 Reset 버튼을 누르세요</div>
             </div>
         </>
     )
