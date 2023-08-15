e79ed403a9eb43d795447b5b603fc89e13d556f9
Phabala <kihong2424@gmail.com>
2023-08-15 13:11:27 +0900

    [STYLE] 제스처 설명 CSS, 리셋버튼 크기 수정

--- frontend/src/component/Gesture/TestExplain.js
+++ frontend/src/component/Gesture/TestExplain.js
@@ -7,7 +7,7 @@ function TestExplain() {
         <>
             <div className="test-explain">
                 <div className="test-explain-text">
-                    <div>👌 오케이 표시로 타이머를 시작해 보아요!</div>
+                    <div>👌 오케이 표시로 타이머를 시작하세요!</div>
                     <div>✅ 완료되면 엄지와 검지를 펴서 체크업 하세요!</div>
                     <div>✋ 질문이 있으면 손을 번쩍 들어 질문하세요!</div>
                     {/* <div>(설명 CSS변경, 홈 되돌아가기 버튼 추가 예정)</div> */}
