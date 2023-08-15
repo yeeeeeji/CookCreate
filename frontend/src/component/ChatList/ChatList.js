6986fe2d6f758b03c529a275db30481b4314f632
hwnagsua <tsmich926@naver.com>
2023-08-15 21:39:29 +0900

    [STYLE]chatlist css수정 #S09P12C111-306

--- frontend/src/component/ChatList/ChatList.js
+++ frontend/src/component/ChatList/ChatList.js
@@ -13,7 +13,7 @@ const ChatListContainer = styled.div`
   // margin-right: 1px;
 
   flex: none;
-  height: 582px; 
+  height: 512px; 
   width: 230px;
   padding: 20px;
   // background-color: rgb(255 239 221);
@@ -24,42 +24,59 @@ const ChatListContainer = styled.div`
 
 const StyledChatH3 = styled.h3`
   font-size: 17px;
-  color: rgb(87, 49, 14);
+  color: #414141;
   text-align: center; 
 `;
 
-// const StyledChatH2 = styled.h2`
-//   font-size: 15px;
-//   color: rgb(87, 49, 14);
-//   text-align: center; 
-// `;
 
+const Wrapper = styled.header`
+  width: 100%;
+  border: 1px solid #D9D9D9;
+  // background-color: #ff8a00;
+  // background-color: #ff9416;
+  background-color: #FFB697;
+  height: 70px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+  flex-direction: row-reverse;
 
-// const StyledDivider = styled.div`
-//   border-top: 0.5px solid #ccc;
-//   margin: 10px 0;
-// `;
+  & span {
+    color:#FFFFFF;
+    font-weight: bold;
+    font-size: 25px;
+    // margin-right: 50px;
+    text-align: center;
+    flex-grow: 1;
+    justify-content: center;
+    display:flex;
+    // margin-left: 60px;
 
-// const ChatListContainer = styled.div`
-//   background-color: #ffffff;
-//   border: 1px solid #D9D9D9;
-//   flex: none;
-//   height: 582px; 
-//   width: 230px;
-//   padding: 20px;
-//   overflow-y: auto;
-//   margin-right: 1px;
-// `;
+  }
 
+  & button {
+    align-items: center;
+    justify-content: center;
+    display:flex;
+    width: 60px;
+    height: 30px;
 
-
-// const StyledChatH3 = styled.h3`
-//   font-size: 17px;
-//   color: rgb(131 126 122);
-//   text-align: center;
-//   margin: 0;
-// `;
-
+    font-size: 15px;
+    background-color: #FF7A42;
+    // background-color: #ff9416;
+    // background-color: #FF7A42;
+    outline: none;
+    cursor: pointer;
+    border-radius: 3px;
+    border: none;
+    color: wheat;
+    // &:hover {
+    //   background-color: #FF7A42;
+    // }
+  }
+`;
 
 
 
@@ -67,6 +84,8 @@ const ChatList = (props) => {
   console.log("props", props);
   const accessToken = localStorage.getItem("access_token");
   const [chatlist, setChatList] = useState([]);
+  const [selectedChatRoom, setSelectedChatRoom] = useState([]);
+
 
   useEffect(() => {
     console.log(chatlist);
@@ -94,26 +113,35 @@ const ChatList = (props) => {
   const handleChatRoom = ({ chatTitle, lessonId, chatOver }) => {
     console.log(chatTitle, lessonId, chatOver);
     props.goChatRoom({ chatTitle, lessonId, chatOver });
+    // setSelectedChatRoom(chatRoom.lessonId)}
+    // onMouseEnter={() => setSelectedChatRoom(chatRoom.lessonId)}
+    // onMouseLeave={() => setSelectedChatRoom(null)}
   };
+
+
+  // const selectRoom = (lessonId) => {
+  //   setSelectedChatRoom(lessonId);
+  // }
 
   return (
     <div>
+      <Wrapper>
+      </Wrapper>
       <ChatListContainer>
-        {/* <h3 className="ChatH3" style={{ fontSize: 17, color: 'rgb(87, 49, 14)' }}>참여중인 채팅방</h3> */}
+        <StyledChatH3>참여중인 채팅방</StyledChatH3>
         <div>
-          <StyledChatH3>참여중인 채팅방</StyledChatH3>
           {chatlist.map((chatRoom) => (
             <div key={chatRoom.lessonId}>
               {chatRoom.chatRoomOver === false && (
                 <>
-                  <div style={{  marginLeft: '40px' }} onClick={() => handleChatRoom({ lessonId: chatRoom.lessonId, chatTitle: chatRoom.lessonTitle, chatOver: chatRoom.chatRoomOver })}>
+                  <div style={{  marginLeft: '40px'}} onClick={() => handleChatRoom({ lessonId: chatRoom.lessonId, chatTitle: chatRoom.lessonTitle, chatOver: chatRoom.chatRoomOver })}>
                     {/* <Link to={`/chatroom`}> */}
                     {chatRoom.lessonTitle ? <strong style={{ color: '#414141' }} >{chatRoom.lessonTitle}</strong> : <p>참여중인 채팅방이 없습니다.</p>}
                     {chatRoom.leastContent !== null ? (
                       <div>
-                        <p style={{ color:'#B5B4B4'}} >{chatRoom.leastContent.length > 10 ? chatRoom.leastContent.slice(0, 7) + ".." : chatRoom.leastContent}</p>
+                        <p style={{ color:'#B5B4B4'}} >{chatRoom.leastContent.length > 10 ? chatRoom.leastContent.slice(0, 12) + ".." : chatRoom.leastContent}</p>
                         {/* <span>{formatTime(chatRoom.lestCreateTime)}</span> */}
-                        <span style={{ fontSize: "10px", color:'rgb(92 82 73)' }}>
+                        <span style={{ fontSize: "10px", color:'#B5B4B4' }}>
                           {new Date(chatRoom.lestCreateTime).toLocaleDateString("ko-KR", {
                             month: "2-digit",
                             day: "2-digit",
@@ -139,9 +167,9 @@ const ChatList = (props) => {
                     <strong style={{ color: '#414141' }} >{chatRoom.lessonTitle}</strong>
                     {/* <h2>{chatRoom.lessonTitle}</h2> */}
                     {/* <p>마지막 메세지: {chatRoom.leastContent}</p> */}
-                    <p style={{ color:'#B5B4B4' }}>{chatRoom.leastContent.length > 10 ? chatRoom.leastContent.slice(0, 10) + "..." : chatRoom.leastContent}</p>
+                    <p style={{ color:'#B5B4B4' }}>{chatRoom.leastContent.length > 10 ? chatRoom.leastContent.slice(0, 12) + "..." : chatRoom.leastContent}</p>
                     {/* <span>종료 시간: {formatTime(chatRoom.lestCreateTime)}</span> */}
-                    <span style={{ fontSize: "10px",color:'rgb(92 82 73)' }}>
+                    <span style={{ fontSize: "10px",color:'#B5B4B4' }}>
                       {new Date(chatRoom.lestCreateTime).toLocaleDateString("ko-KR", {
                         month: "2-digit",
                         day: "2-digit",
