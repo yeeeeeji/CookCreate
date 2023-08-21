00e1a72010de9ef8b3d9f624785275cde06a2cfa
조희라 <bunny7531@naver.com>
2023-08-21 13:51:45 +0900

    [CSS] 리뷰 css 수정

--- frontend/src/component/LessonDetail/LessonReview.js
+++ frontend/src/component/LessonDetail/LessonReview.js
@@ -11,6 +11,22 @@ function LessonReview() {
   const [Reviews, setReveiws] = useState([]);
   const [cookyerId, setCookyerId] = useState(null);
 
+  const displayTime = (dateTime) => {
+    if (!dateTime) return null;
+
+    const localDate = new Date(dateTime);
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZone: "Asia/Seoul", // 시간대를 UTC로 설정 (한국 시간으로 하는게 맞는 거 같다)
+    };
+    return localDate.toLocaleString("ko-KR", options)
+    .replace(". ","-").replace(". ","-").replace("."," ");
+  };
+
   useEffect(() => {
     if (ReviewGrade) {
       setCookyerId(ReviewGrade.cookyerId);
@@ -60,6 +76,9 @@ function LessonReview() {
 
   console.log("리뷰리스트", Reviews);
 
+  const previewImage =
+  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+
   return (
     <div className="review-container">
       <div className="review-header">
@@ -67,7 +86,7 @@ function LessonReview() {
         {ReviewGrade.reviewCnt > 0 ? (
           <div className="review-avg-container">
             <div className="review-star">
-              <DecimalStar rating={ReviewGrade.reviewAvg} />
+              <DecimalStar rating={ReviewGrade.reviewAvg} iconSize="20px" />
             </div>
             <div className="review-avg">
               <div>{ReviewGrade.reviewAvg.toFixed(1)}/5.0</div>
@@ -83,8 +102,27 @@ function LessonReview() {
           <div>
             {Reviews.map((content, reviewId) => (
               <div key={reviewId} className="review-item">
-                <div className="review-nickname">{content.nickname}</div>
-                <div className="review-content">{content.reviewContents}</div>
+                <div className="review-info-wrap">
+                <div>
+                  {content.profileImg ? (
+            <img className="review-profile-img" src={content.profileImg} alt="" />
+          ) : (
+            <img
+              className="review-profile-img"
+              src={previewImage}
+              alt=""
+            />
+          )}{" "}  
+                </div>
+                <div className="review-text-wrap">
+                  <div className="review-item-star"><DecimalStar rating={content.rating} iconSize="15px"/></div>
+                  <div className="review-nickname">{content.nickname}</div>
+                </div>
+                </div>
+                <div>
+                  <div className="review-content">{content.reviewContents}</div>
+                  <div className="review-date">{displayTime(content.modifiedDate)}</div>
+                </div>
               </div>
             ))}
           </div>
