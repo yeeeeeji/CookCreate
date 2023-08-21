03c238b1593ac7766654c65a7abe84655a93f7ff
윤예지 <hallo27@naver.com>
2023-08-21 13:21:54 +0900

    [MODIFY] 쿠키 화상과외 리뷰 등록 모달 추가

--- frontend/src/component/Video/Cookiee/LessonReviewModal.js
+++ frontend/src/component/Video/Cookiee/LessonReviewModal.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LessonReviewContent from './LessonReviewContent';
+import AlertModal from '../../Modal/AlertModal';
 
 function LessonReviewModal() {
+  const [ showModal, setShowModal ] = useState(false)
+  const [ failModal, setFailModal ] = useState(false)
 
   return (
     <div className='cookiee-lesson-review-modal'>
+      {showModal && <AlertModal content='리뷰가 등록되었습니다.' path={-1}/>}
+      {failModal && <AlertModal content='리뷰 등록에 실패했습니다. 마이페이지에서 다시 확인해주세요.' path={-1}/>}
       <div className='cookyer-lesson-step-modal-title-wrapper'>
         <p className='cookyer-lesson-step-modal-title'>리뷰 쓰기</p>
       </div>
       <div>
-        <LessonReviewContent/>
+        <LessonReviewContent setShowModal={setShowModal} setFailModal={setFailModal}/>
       </div>
     </div>
   );
