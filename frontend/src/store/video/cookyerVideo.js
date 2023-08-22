fc9bc3036a61b695e5a217d09188bf8e1a4653ef
윤예지 <hallo27@naver.com>
2023-08-22 10:43:08 +0900

    [FIX] 화상과외 도중 쿠키 퇴장시 손들기 및 체크 리스트에서 제거 안 되는 문제 해결

--- frontend/src/store/video/cookyerVideo.js
+++ frontend/src/store/video/cookyerVideo.js
@@ -42,7 +42,7 @@ export const cookyerVideo = createSlice({
     deleteCookiee: (state, { connectionId }) => {
       console.log("그냥 나간 쿠키 리스트에서 제거됐니")
       /** 손 든 참가자 리스트에서 제외 */
-      if (state.handsUpCookieeList !== undefined) {
+      if (state.handsUpCookieeList) {
         const newHandsUpCookieeList = state.handsUpCookieeList.filter((item) => {
           return item !== connectionId
         })
@@ -51,9 +51,9 @@ export const cookyerVideo = createSlice({
       }
 
       /** 체크한 참가자 리스트에서 제외 */
-      if (state.checkCookieeList !== undefined) {
+      if (state.checkCookieeList) {
         const newCheckCookieeList = state.checkCookieeList.filter((item) => {
-          return item !== state.uncheckCookiee
+          return item !== connectionId
         })
         state.checkCookieeList = newCheckCookieeList
         console.log(newCheckCookieeList, '체크 해제한 사람 제외 새 체크 리스트')
